65b3d548fefce74a6cfcee4220008bb9bd7cbef1
MarkRBishop <mrb.8@hotmail.com>
2023-12-13 18:20:20 -0600

    removed unnecessary code

--- server.js
+++ server.js
@@ -14,10 +14,6 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'notes.html'))
 })
 
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'index.html'))
-// })
-
 app.get('/api/notes', (req, res) => {
    fs.readFile('./db/db.json', 'utf8', (err, data) => {
         if (err) {
